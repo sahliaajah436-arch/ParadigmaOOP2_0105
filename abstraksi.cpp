0675a01e63a02d5b5a3eb895238648ce25ee4b1a
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-05-19 18:30:29 +0700

    method untuk mengisi nilai dan private member

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -3,5 +3,16 @@ using namespace std;
 
 class AbstraksiKlas{
 private:
+    string x, y;
+
+public:
+
+    // method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+
 
 }
